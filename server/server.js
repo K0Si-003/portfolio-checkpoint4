f891c99996ba3cb2051d8b1337cd588165c8d2ae
K0Si-003 <hugo.pioda@gmail.com>
2020-07-23 11:10:57 +0200

    fetch data

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 // routes
 app.use('/tags', require('./routes/tag.routes.js'));
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import API from '../API';
+import ProjectDetails from './ProjectDetails';
 
 const Project = () => {
-  const [tags, setTags] = useState();
+  const [tags, setTags] = useState([]);
 
-useEffect(() => {
-  API.get('/tags')
-    .then(res => res.data)
-    .then(data => {
-      console.log(data);
-      setTags(data);
-    })
-}, []);
+  useEffect(() => {
+    API.get('/tags')
+      .then(res => res.data)
+      .then(data => setTags(data))
+  }, []);
 
-  return (
-    <div>
-      <h1>Projects</h1>
-    </div>
-  )
+  if (!tags) {
+    return <p>Loading tags...</p>
+  } else {
+    return (
+      <div>
+        <h1>Projets</h1>
+        {tags.map(t => {
+          return <ProjectDetails key={t.id} apartmentDetails={t} />;
+        })}
+      </div>
+    )
+  }
 }
 
 export default Project;
