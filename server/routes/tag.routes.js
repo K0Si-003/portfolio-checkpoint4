03fe87e622e80b8ab13924adf5b572bfe217327d
K0Si-003 <hugo.pioda@gmail.com>
2020-07-23 16:53:15 +0200

    clean master

--- server/routes/tag.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const tagsController = require('../controllers/tag.controller.js');
-
-router.get('/', tagsController.findAll);
-
-module.exports = router;
